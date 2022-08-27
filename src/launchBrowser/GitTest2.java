8fd80a744ac246206b0bf148424853067b0ce263
WasifRafee <wasifrafee@gmail.com>
2022-08-27 17:53:21 -0500

    3rd commit

---
+++ src/launchBrowser/GitTest2.java
@@ -0,0 +1,23 @@
+package launchBrowser;
+
+public class GitTest2 {
+	public static void main(String[] args) {
+		printme1();
+		printme2();
+	}
+	
+	public static void printme1() {
+		System.out.println("git test");
+	}
+
+
+	public static void printme2() {
+		System.out.println("git test2");
+		System.out.println("git test3");
+		System.out.println("git test4");
+		System.out.println("git test5");
+	}
+	
+
+
+}
